461cb46a8fa5db1578916b9dad85a9e40db24c9d
pwput <45051480+pwput@users.noreply.github.com>
2022-12-07 14:40:13 +0100

    Working for two answers dialog
    need to figure out inherance or something for three answers dialog

--- src/main/java/com/sample/DroolsMain.java
+++ src/main/java/com/sample/DroolsMain.java
@@ -4,20 +4,15 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
-/**
- * This is a sample file to launch a process.
- */
-public class ProcessTest {
+public class DroolsMain {
 
     public static final void main(String[] args) {
         try {
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-process");
-
-            // start a new process instance
-            kSession.startProcess("com.sample.bpmn.hello");
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+            kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
