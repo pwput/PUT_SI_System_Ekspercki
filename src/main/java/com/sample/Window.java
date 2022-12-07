2f604ee485a506f938c4fe2f071029e570c0659c
pwput <45051480+pwput@users.noreply.github.com>
2022-12-07 17:00:28 +0100

    Working system, tree cropped now I have to write all rules

--- src/main/java/com/sample/Window.java
+++ src/main/java/com/sample/Window.java
@@ -1,41 +1,24 @@
 package com.sample;
-import javax.swing.Icon;
 import javax.swing.JOptionPane;
 
 public class Window {
 	
-	
 	public Window(){}
 	
-	public static String askUser(String question, String answer) {
-		String[] options = {answer};
-		return showDialog(question, options);
-	}
-	
-	public static String askUser(String question, String firstAnswer, String secondAnswer, String thirdAnswer) {
-		String[] options = {firstAnswer,secondAnswer,thirdAnswer};
-		return showDialog(question, options);
-	}
-	
-	public static String askUser(String question, String firstAnswer, String secondAnswer) {
-		String[] options = {firstAnswer,secondAnswer};
-		return showDialog(question, options);
-    }
-	
-	public static String showDialog(String question, String[] options) {
+	public static String askUser(String question, String[] options) {
 		int n = JOptionPane.showOptionDialog(
-			null,							//parent
-			question,						//Dialog message
-			"StarWars_static",				//Dialog title
-			JOptionPane.YES_NO_OPTION,		//option type
-			JOptionPane.QUESTION_MESSAGE,	//message type
-			null,     						//icon
-			options,  						//options
-			options[0]					//initial value
-		); 
-		if (n<0 || n >= options.length)
-			return "User interaption";
-		else 
-			return options[n];
-	}	
+				null,							//parent
+				question,						//Dialog message
+				"StarWars_static",				//Dialog title
+				JOptionPane.YES_NO_OPTION,		//option type
+				JOptionPane.QUESTION_MESSAGE,	//message type
+				null,     						//icon
+				options,  						//options
+				options[0]					//initial value
+			); 
+			if (n<0 || n >= options.length)
+				return "User interaption";
+			else 
+				return options[n];
+		}			
 }
